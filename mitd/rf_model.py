49df76c6c9c2d096f635059bdb3643b18e7bf525
garyjsk271 <garyjsk271@gmail.com>
2021-10-20 18:10:22 -0700

    file separation
    function template for svm_model.py is more or less complete.
    
    As soon as image conversion is done, we can proceed to svm_model.py (which should really take only a day or two)

---
+++ mitd/rf_model.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 20 18:00:23 2021
+
+@author: GJK
+"""
+
+class RandomForest_Model:
+    def __init__(self):
+        self.__model        = None
+        self.__groundTruths = []
+        self.__predictions  = []
+    
+    """
+        TODO
+        Uses the current SVM model to classify all test instances.
+        @param dbTest list containing all test instances.
+        
+        @note
+            __groundTruths shall be populated with the ground truths in dbTest.
+            __predictions shall be populated with all the predictions made. 
+    """
+    def predict(dbTest):
+        return
+    
+    """
+        Calculates the accuracy of the model.
+        @return accuracy the accuracy (type float) of the model based on the 
+            predictions made against the ground truths.
+    """
+    def accuracy():
+        accuracy = metrics.accuracy_score(self.__groundTruths, self.__predictions)
+        return accuracy
